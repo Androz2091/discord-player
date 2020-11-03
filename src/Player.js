591881f95fdf10f1c1277782be4a559fc35ffd38
LePtitMetalleux <59097429+LePtitMetalleux@users.noreply.github.com>
2020-11-03 20:28:52 +0100

    Fix noResult event

--- src/Player.js
+++ src/Player.js
@@ -171,7 +171,7 @@ class Player extends EventEmitter {
                 }).catch(() => {})
             }
 
-            if (tracks.length === 0) this.emit('noResults', message, query)
+            if (tracks.length === 0) return this.emit('noResults', message, query)
 
             this.emit('searchResults', message, query, tracks)
 
