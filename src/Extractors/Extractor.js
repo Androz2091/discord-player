aca912f12417031743cab7b18acfe5187ae6acab
Snowflake107 <devsnowflake107@gmail.com>
2021-01-11 12:17:41 +0545

    vimeo extractor

--- src/Extractors/Extractor.js
+++ src/Extractors/Extractor.js
@@ -1,4 +1,5 @@
 module.exports = {
     DiscordExtractor: require('./Discord'),
-    FacebookExtractor: require('./Facebook')
+    FacebookExtractor: require('./Facebook'),
+    VimeoExtractor: require('./Vimeo')
 }
