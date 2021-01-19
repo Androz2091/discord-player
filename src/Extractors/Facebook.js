54a6ab040b8937524a052a652143548c23d0a232
Snowflake107 <devsnowflake107@gmail.com>
2021-01-19 19:17:17 +0545

    fix duration

--- src/Extractors/Facebook.js
+++ src/Extractors/Facebook.js
@@ -117,8 +117,37 @@ class Facebook {
      */
     static parseTime (duration) {
         if (typeof duration !== 'string') return duration
-        duration = duration.toUpperCase().replace('T', '')
-        return duration.split('').map((x, y) => !parseInt(x) ? (y === duration.length - 1 ? '' : ':') : x).join('')
+        let a = duration.match(/\d+/g)
+
+        if (duration.indexOf('M') >= 0 && duration.indexOf('H') === -1 && duration.indexOf('S') === -1) {
+            a = [0, a[0], 0]
+        }
+
+        if (duration.indexOf('H') >= 0 && duration.indexOf('M') === -1) {
+            a = [a[0], 0, a[1]]
+        }
+        if (duration.indexOf('H') >= 0 && duration.indexOf('M') === -1 && duration.indexOf('S') === -1) {
+            a = [a[0], 0, 0]
+        }
+
+        duration = 0
+
+        if (a.length === 3) {
+            duration = duration + parseInt(a[0]) * 3600
+            duration = duration + parseInt(a[1]) * 60
+            duration = duration + parseInt(a[2])
+        }
+
+        if (a.length === 2) {
+            duration = duration + parseInt(a[0]) * 60
+            duration = duration + parseInt(a[1])
+        }
+
+        if (a.length === 1) {
+            duration = duration + parseInt(a[0])
+        }
+
+        return duration
     }
 
     /**
