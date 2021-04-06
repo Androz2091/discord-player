fc8d0d08eeadf7177c2efe0e663cd02f3d5f1e84
Snowflake107 <devsnowflake107@gmail.com>
2021-04-06 20:09:42 +0545

    export everything

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export * as AudioFilters from './utils/AudioFilters';
+export * as Constants from './utils/Constants';
 export * as Player from './Player';
 export * as Util from './utils/Util';
 export * as Track from './Structures/Track';
 export * as Queue from './Structures/Queue';
 export * from './types/types';
+export * as PlayerError from './utils/PlayerError';
 export { version } from '../package.json';
