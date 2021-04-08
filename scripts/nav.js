59305a49decf0295960142e9fad87e0c6e77b414
ToledoSDL <65132614+ToledoSDL@users.noreply.github.com>
2021-04-08 12:19:34 -0300

    Formatting the code

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,31 +1,31 @@
-const init = async () => {
-    window.addEventListener("DOMContentLoaded", function () {
-        const burger = document.querySelector(".burger"),
-            nav = document.querySelector(".navbar"),
-            navLinks = nav.querySelectorAll("a");
-        
-        burger.addEventListener("click", function () {
-            toggleClass(burger, "is-active");
-            toggleClass(nav, "is-active");
-        });
+const init = async() => {
+	window.addEventListener("DOMContentLoaded", function () {
+		const burger = document.querySelector(".burger"),
+			nav = document.querySelector(".navbar"),
+			navLinks = nav.querySelectorAll("a");
 
-        navLinks.forEach(link => {
-            link.addEventListener("click", function () {
-                if (burger.classList.contains("is-active")) {
-                    toggleClass(burger, "is-active");
-                    toggleClass(nav, "is-active");
-                }
-            });
-        });
-    });
+		burger.addEventListener("click", function () {
+			toggleClass(burger, "is-active");
+			toggleClass(nav, "is-active");
+		});
 
-    function toggleClass(element, className) {
-        if (element.classList.contains(className)) {
-            element.classList.remove(className);
-        } else {
-            element.classList.add(className);
-        }
-    }
+		navLinks.forEach(link => {
+			link.addEventListener("click", function () {
+				if (burger.classList.contains("is-active")) {
+					toggleClass(burger, "is-active");
+					toggleClass(nav, "is-active");
+				}
+			});
+		});
+	});
+
+	function toggleClass(element, className) {
+		if (element.classList.contains(className)) {
+			element.classList.remove(className);
+		} else {
+			element.classList.add(className);
+		}
+	}
 }
 
 init();
