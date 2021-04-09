8c5014630083b989d80fe87bc7c8abdea40336a8
Snowflake107 <devsnowflake107@gmail.com>
2021-04-09 14:49:30 +0545

    export ExtractorModel

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { AudioFilters } from './utils/AudioFilters';
 export * as Constants from './utils/Constants';
+export { ExtractorModel } from './Structures/ExtractorModel';
 export { Player } from './Player';
 export { Util } from './utils/Util';
 export { Track } from './Structures/Track';
