41490b56374a2f6d4921e0ade7510149187dd015
Snowflake107 <devsnowflake107@gmail.com>
2021-05-01 13:32:47 +0545

    progressbar track

--- src/Player.ts
+++ src/Player.ts
@@ -882,7 +882,7 @@ export class Player extends EventEmitter {
 
         const index = Math.round((currentStreamTime / totalTime) * length);
         const indicator = typeof options?.indicator === 'string' && options?.indicator.length > 0 ? options?.indicator : '🔘';
-        const line = typeof options?.line === 'string' ? options?.line.length > 0 ? options?.line : '▬' : '▬';
+        const line = typeof options?.line === 'string' && options?.line.length > 0 ? options?.line : '▬';
 
         if (index >= 1 && index <= length) {
             const bar = line.repeat(length - 1).split('');
