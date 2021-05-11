8d45b126749969d6956b8d53fd2d33e519f70721
Snowflake107 <devsnowflake107@gmail.com>
2021-05-11 19:54:13 +0545

    env utils

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,21 +6,23 @@ import { YouTube } from 'youtube-sr';
  * Utility env
  */
 export class Env {
-
     /**
      * The soundcloud client
+     * @example const sc = new (require("discord-player")).Env.SoundcloudClient.Client();
+     * sc.search("faded").then(console.log);
      */
     static get SoundcloudClient() {
-        return Client;
+        return { Client };
     }
 
     /**
      * The soundcloud store utils
+     * @example const store = require("discord-player").Env.SoundcloudStore.setAPIkey("soundcloud_api_key")
      */
     static get SoundcloudStore() {
         const store = Store as Map<string, string>;
         const setAPIkey = (newKey: string) => store.set('SOUNDCLOUD_API_KEY', newKey);
-        return { setAPIkey };
+        return { setAPIkey, store };
     }
 
     /**
