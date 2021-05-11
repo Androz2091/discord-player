1ca22167efb4c66959b28485f2dfa016879db988
Snowflake107 <devsnowflake107@gmail.com>
2021-05-11 19:50:00 +0545

    player env utils

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,17 +2,30 @@
 import { Store, Client } from 'soundcloud-scraper';
 import { YouTube } from 'youtube-sr';
 
+/**
+ * Utility env
+ */
 export class Env {
+
+    /**
+     * The soundcloud client
+     */
     static get SoundcloudClient() {
         return Client;
     }
 
+    /**
+     * The soundcloud store utils
+     */
     static get SoundcloudStore() {
         const store = Store as Map<string, string>;
         const setAPIkey = (newKey: string) => store.set('SOUNDCLOUD_API_KEY', newKey);
         return { setAPIkey };
     }
 
+    /**
+     * The youtube search
+     */
     static get youtube() {
         return YouTube;
     }
