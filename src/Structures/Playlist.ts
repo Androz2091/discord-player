5d5fd03997f777326b2a84fb2482ea53afb7a24e
Snowflake107 <devsnowflake107@gmail.com>
2021-06-11 16:20:04 +0545

    feat(Structures): basic setup

--- src/Structures/Playlist.ts
+++ src/Structures/Playlist.ts
@@ -1,1 +1,15 @@
-export {};
+import { Player } from "../Player";
+import { Track } from "./Track";
+
+class Playlist {
+    public readonly player: Player;
+    public tracks: Track[];
+
+    constructor(player: Player, tracks: Track[]) {
+        this.player = player;
+        this.tracks = tracks ?? [];
+    }
+
+}
+
+export { Playlist };
