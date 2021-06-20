621bd1c274d004676fb5b3cc152466a2bb4725c6
Snowflake107 <devsnowflake107@gmail.com>
2021-06-20 14:59:47 +0545

    fix types

--- src/types/types.ts
+++ src/types/types.ts
@@ -38,8 +38,8 @@ export type QueueFilters = {
     chorus2d?: boolean;
     chorus3d?: boolean;
     fadein?: boolean;
-    dim?: string;
-    earrape?: string;
+    dim?: boolean;
+    earrape?: boolean;
 };
 
 export type TrackSource = "soundcloud" | "youtube" | "spotify" | "arbitrary";
