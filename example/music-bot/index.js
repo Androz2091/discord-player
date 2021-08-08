aecb61f1746203d7daf51fb0c687023bc88da815
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-08-08 12:13:15 +0545

    fix(Player): get correct info for youtube video links

--- example/music-bot/index.js
+++ example/music-bot/index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const { Client, GuildMember } = require("discord.js");
 const config = require("./config");
-const { Player, QueryType, QueueRepeatMode } = require("discord-player");
+// const { Player, QueryType, QueueRepeatMode } = require("discord-player");
+const { Player, QueryType, QueueRepeatMode } = require("../../");
 
 const client = new Client({
     intents: ["GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILDS"]
