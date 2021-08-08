481f4f715a0cb54f986db6add77124c783f5c2b0
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-08-08 11:52:35 +0545

    feat: expose library version

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ export { PlayerError, ErrorStatusCode } from "./Structures/PlayerError";
 export { QueryResolver } from "./utils/QueryResolver";
 export { Queue } from "./Structures/Queue";
 export { Track } from "./Structures/Track";
-export { Util } from "./utils/Util";
 export { VoiceUtils } from "./VoiceInterface/VoiceUtils";
 export { VoiceEvents, StreamDispatcher } from "./VoiceInterface/StreamDispatcher";
+export { Util } from "./utils/Util";
 export * from "./types/types";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+export const version: string = require(`${__dirname}/../package.json`).version;
