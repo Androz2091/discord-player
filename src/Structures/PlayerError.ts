bfd206d5df31b9dd00d3d70116b5232dae2464e2
and <46562212+DevAndromeda@users.noreply.github.com>
2021-11-28 13:43:35 +0545

    refactor(PlayerError): return message on calling toJSON

--- src/Structures/PlayerError.ts
+++ src/Structures/PlayerError.ts
@@ -39,7 +39,12 @@ export class PlayerError extends Error {
     }
 
     toJSON() {
-        return { stack: this.stack, code: this.statusCode, created: this.createdTimestamp };
+        return {
+            stack: this.stack,
+            code: this.statusCode,
+            message: this.message,
+            created: this.createdTimestamp
+        };
     }
 
     toString() {
