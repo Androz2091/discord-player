122e0976718d32d7d437768b478c17297f85664a
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2022-07-19 20:54:23 +0545

    📝 Do not erase doc file

--- scripts/docgen.js
+++ scripts/docgen.js
@@ -1,11 +1,8 @@
 /* eslint-disable */
 const { runGenerator } = require("@discordjs/ts-docgen");
-const fs = require("node:fs");
 
 runGenerator({
     existingOutput: "docs/typedoc.json",
     custom: "docs/config.yml",
     output: "docs/docs.json"
 });
-
-fs.unlinkSync(`${__dirname}/../docs/typedoc.json`);
