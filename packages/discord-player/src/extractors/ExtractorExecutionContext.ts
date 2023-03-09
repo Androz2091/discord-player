d0eafd9b16adf857e5646b46444da2b6761ce508
skdhg <46562212+skdhg@users.noreply.github.com>
2023-03-09 20:55:59 +0545

    feat(ExtractorExecutionContext): extractor events

--- packages/discord-player/src/extractors/ExtractorExecutionContext.ts
+++ packages/discord-player/src/extractors/ExtractorExecutionContext.ts
@@ -2,6 +2,7 @@ import { Player } from '../Player';
 import { Collection } from '@discord-player/utils';
 import { BaseExtractor } from './BaseExtractor';
 import { Util } from '../utils/Util';
+import { PlayerEventsEmitter } from '../utils/PlayerEventsEmitter';
 
 // prettier-ignore
 const knownExtractorKeys = [
@@ -15,9 +16,44 @@ const knownExtractorKeys = [
 ];
 const knownExtractorLib = '@discord-player/extractor';
 
-export class ExtractorExecutionContext {
+export interface ExtractorExecutionEvents {
+    /**
+     * Emitted when a extractor is registered
+     * @param context The context where extractor was registered
+     * @param extractor The extractor that was registered
+     */
+    registered: (context: ExtractorExecutionContext, extractor: BaseExtractor) => unknown;
+    /**
+     * Emitted when a extractor is unregistered
+     * @param context The context where extractor was unregistered
+     * @param extractor The extractor that was unregistered
+     */
+    unregistered: (context: ExtractorExecutionContext, extractor: BaseExtractor) => unknown;
+    /**
+     * Emitted when a extractor is activated
+     * @param context The context where this event occurred
+     * @param extractor The extractor which was activated
+     */
+    activate: (context: ExtractorExecutionContext, extractor: BaseExtractor) => unknown;
+    /**
+     * Emitted when a extractor is deactivated
+     * @param context The context where this event occurred
+     * @param extractor The extractor which was deactivated
+     */
+    deactivate: (context: ExtractorExecutionContext, extractor: BaseExtractor) => unknown;
+    /**
+     * Emitted when a extractor fails to activate/deactivate
+     * @param context The context where this event occurred
+     * @param extractor The extractor which was deactivated
+     */
+    error: (context: ExtractorExecutionContext, extractor: BaseExtractor, error: Error) => unknown;
+}
+
+export class ExtractorExecutionContext extends PlayerEventsEmitter<ExtractorExecutionEvents> {
     public store = new Collection<string, BaseExtractor>();
-    public constructor(public player: Player) {}
+    public constructor(public player: Player) {
+        super(['error']);
+    }
 
     public async loadDefault() {
         const mod = await Util.import(knownExtractorLib);
@@ -50,11 +86,14 @@ export class ExtractorExecutionContext {
         try {
             this.store.set(_extractor.identifier, extractor);
             this.player.debug(`${_extractor.identifier} extractor loaded!`);
+            this.emit('registered', this, extractor);
             await extractor.activate();
             this.player.debug(`${_extractor.identifier} extractor activated!`);
+            this.emit('activate', this, extractor);
         } catch (e) {
             this.store.delete(_extractor.identifier);
             this.player.debug(`${_extractor.identifier} extractor failed to activate! Error: ${e}`);
+            this.emit('error', this, extractor, e as Error);
         }
     }
 
@@ -66,21 +105,19 @@ export class ExtractorExecutionContext {
             const key = extractor.identifier || this.store.findKey((e) => e === extractor)!;
             this.store.delete(key);
             this.player.debug(`${extractor.identifier} extractor disabled!`);
+            this.emit('unregistered', this, extractor);
             await extractor.deactivate();
             this.player.debug(`${extractor.identifier} extractor deactivated!`);
-        } catch {
+            this.emit('deactivate', this, extractor);
+        } catch (e) {
             this.player.debug(`${extractor.identifier} extractor failed to deactivate!`);
+            this.emit('error', this, extractor, e as Error);
         }
     }
 
     public async unregisterAll() {
         try {
-            await Promise.all(
-                this.store.map((e, k) => {
-                    this.store.delete(k);
-                    return e.deactivate();
-                })
-            );
+            await Promise.all(this.store.map((e) => this.unregister(e)));
         } catch {
             // do nothing
         }
