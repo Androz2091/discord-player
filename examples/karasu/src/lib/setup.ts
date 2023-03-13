42e67368cb5e5dd5677ace75b9cfa245478ad332
Abdul Rahim <abdulrahimshoid@gmail.com>
2023-03-13 18:46:04 +0000

    refactor(karasu): setup

--- examples/karasu/src/lib/setup.ts
+++ examples/karasu/src/lib/setup.ts
@@ -1,24 +1,12 @@
-// Unless explicitly defined, set NODE_ENV as production:
-process.env.NODE_ENV ??= 'production';
-
-import '@sapphire/plugin-hmr/register';
 import '@sapphire/plugin-api/register';
-import '@sapphire/plugin-editable-commands/register';
+import '@sapphire/plugin-hmr/register';
 import '@sapphire/plugin-logger/register';
 import { setup, type ArrayString } from '@skyra/env-utilities';
-import * as colorette from 'colorette';
 import { join } from 'path';
-import { inspect } from 'util';
-import { srcDir } from './constants';
 
-// Read env var
+const rootDir = join(__dirname, '..', '..');
+const srcDir = join(rootDir, 'src');
 setup({ path: join(srcDir, '.env') });
-
-// Set default inspection depth
-inspect.defaultOptions.depth = 1;
-
-// Enable colorette
-colorette.createColors({ useColor: true });
 
 declare module '@skyra/env-utilities' {
 	interface Env {
