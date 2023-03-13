e3f7f0774b0bc0a14e7eec047e45484e6a42d8d3
Auric <33577619+itsauric@users.noreply.github.com>
2023-03-13 19:10:39 +0000

    rename(karasu): 'equaliser 'to 'equalizer'

--- examples/karasu/src/commands/equalizer.ts
+++ examples/karasu/src/commands/equalizer.ts
@@ -5,7 +5,7 @@ export class EqualizerCommand extends Command {
 	public constructor(context: Command.Context, options: Command.Options) {
 		super(context, {
 			...options,
-			description: 'The equaliser filter that can be applied to tracks'
+			description: 'The equalizer filter that can be applied to tracks'
 		});
 	}
 
@@ -17,7 +17,7 @@ export class EqualizerCommand extends Command {
 				.addStringOption((option) =>
 					option
 						.setName('preset')
-						.setDescription('The equaliser filter to use')
+						.setDescription('The equalizer filter to use')
 						.addChoices(
 							...Object.keys(EqualizerConfigurationPreset).map((m) => ({
 								name: m,
@@ -41,7 +41,7 @@ export class EqualizerCommand extends Command {
 
 		if (!queue.filters.equalizer)
 			return interaction.reply({
-				content: `${this.container.client.dev.error} | The equaliser filter is **not available** to be used in this queue`,
+				content: `${this.container.client.dev.error} | The equalizer filter is **not available** to be used in this queue`,
 				ephemeral: true
 			});
 
@@ -49,7 +49,7 @@ export class EqualizerCommand extends Command {
 		queue.filters.equalizer.enable();
 
 		return interaction.reply({
-			content: `${this.container.client.dev.success} | **Equaliser** filter has been set to: **\`${preset}\`**`
+			content: `${this.container.client.dev.success} | **Equalizer** filter has been set to: **\`${preset}\`**`
 		});
 	}
 }
