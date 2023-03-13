ce88c0e3b147949f43f9e0e89545bb2486148e0b
Auric <33577619+itsauric@users.noreply.github.com>
2023-03-13 19:08:21 +0000

    update(karasu): correct equalizer command

--- examples/karasu/src/commands/equalizer.ts
+++ examples/karasu/src/commands/equalizer.ts
@@ -1,12 +1,11 @@
 import { Command } from '@sapphire/framework';
 import { EqualizerConfigurationPreset, useQueue } from 'discord-player';
-import { GuildMember } from 'discord.js';
 
 export class EqualizerCommand extends Command {
 	public constructor(context: Command.Context, options: Command.Options) {
 		super(context, {
 			...options,
-			description: 'The equalizer filter that can be applied to tracks'
+			description: 'The equaliser filter that can be applied to tracks'
 		});
 	}
 
@@ -18,7 +17,7 @@ export class EqualizerCommand extends Command {
 				.addStringOption((option) =>
 					option
 						.setName('preset')
-						.setDescription('The equalizer filter to use')
+						.setDescription('The equaliser filter to use')
 						.addChoices(
 							...Object.keys(EqualizerConfigurationPreset).map((m) => ({
 								name: m,
@@ -31,32 +30,26 @@ export class EqualizerCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		if (interaction.member instanceof GuildMember) {
-			const queue = useQueue(interaction.guild!.id);
-			const permissions = this.container.client.perms.voice(interaction, this.container.client);
-			const preset = interaction.options.getString('preset') as string;
+		const queue = useQueue(interaction.guild!.id);
+		const permissions = this.container.client.perms.voice(interaction, this.container.client);
+		const preset = interaction.options.getString('preset') as string;
 
-			if (!queue) return interaction.reply({ content: `${this.container.client.dev.error} | I am not in a voice channel`, ephemeral: true });
-			if (permissions.clientToMember()) return interaction.reply({ content: permissions.clientToMember(), ephemeral: true });
-			if (!queue.currentTrack)
-				return interaction.reply({
-					content: `${this.container.client.dev.error} | There is no track **currently** playing`,
-					ephemeral: true
-				});
-			if (!queue.filters.equalizer)
-				return interaction.reply({
-					content: `${this.container.client.dev.error} | The equalizer filter is not **available** to be used in this queue`,
-					ephemeral: true
-				});
+		if (!queue) return interaction.reply({ content: `${this.container.client.dev.error} | I am **not** in a voice channel`, ephemeral: true });
+		if (!queue.currentTrack)
+			return interaction.reply({ content: `${this.container.client.dev.error} | There is no track **currently** playing`, ephemeral: true });
+		if (permissions.clientToMember()) return interaction.reply({ content: permissions.clientToMember(), ephemeral: true });
 
-			await interaction.deferReply();
-
-			queue.filters.equalizer.setEQ(EqualizerConfigurationPreset[preset]);
-			queue.filters.equalizer.enable();
-
-			return interaction.followUp({
-				content: `${this.container.client.dev.success} | **Equalizer filter** set to: \`${preset}\``
+		if (!queue.filters.equalizer)
+			return interaction.reply({
+				content: `${this.container.client.dev.error} | The equaliser filter is **not available** to be used in this queue`,
+				ephemeral: true
 			});
-		}
+
+		queue.filters.equalizer.setEQ(EqualizerConfigurationPreset[preset]);
+		queue.filters.equalizer.enable();
+
+		return interaction.reply({
+			content: `${this.container.client.dev.success} | **Equaliser** filter has been set to: **\`${preset}\`**`
+		});
 	}
 }
