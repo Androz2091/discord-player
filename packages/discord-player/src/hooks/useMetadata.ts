13e2b388f3b43bb5d4b493a72f14a947e2b7f445
skdhg <46562212+skdhg@users.noreply.github.com>
2023-03-16 22:42:42 +0545

    feat(useMetadata): allow setter fn

--- packages/discord-player/src/hooks/useMetadata.ts
+++ packages/discord-player/src/hooks/useMetadata.ts
@@ -1,11 +1,15 @@
+import { TypeUtil } from '../utils/TypeUtil';
 import { NodeResolvable } from '../Structures';
 import { getQueue } from './common';
 
+type SetterFN<T, P> = (previous: P) => T;
+
 export function useMetadata<T = unknown>(node: NodeResolvable) {
-    const queue = getQueue(node);
-    const setter = (metadata: T | null) => {
+    const queue = getQueue<T>(node);
+    const setter = (metadata: T | SetterFN<T, T>) => {
         if (queue) {
-            queue.setMetadata(metadata);
+            if (TypeUtil.isFunction(metadata)) return queue.setMetadata(metadata(queue.metadata));
+            return queue.setMetadata(metadata);
         }
     };
 
