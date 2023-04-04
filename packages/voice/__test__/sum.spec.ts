a074ab24137c4032e8e6c9ee334e6c576f2da731
skdhg <46562212+skdhg@users.noreply.github.com>
2023-04-04 13:47:32 +0545

    chore: initialize voice package

---
+++ packages/voice/__test__/sum.spec.ts
@@ -0,0 +1,8 @@
+import { add } from '../src';
+import { describe, it, expect } from 'vitest';
+
+describe('Sum', () => {
+    it('should add two numbers', () => {
+        expect(add(2, 2)).toBe(4);
+    });
+});
