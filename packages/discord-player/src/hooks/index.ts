903009f05e631551c213db93b6509144fe70a0a4
skdhg <46562212+skdhg@users.noreply.github.com>
2023-06-14 19:48:30 +0545

    feat(hooks): useVolume, createHook

--- packages/discord-player/src/hooks/index.ts
+++ packages/discord-player/src/hooks/index.ts
@@ -5,3 +5,5 @@ export * from './useMainPlayer';
 export * from './useMetadata';
 export * from './useTimeline';
 export * from './stream';
+export * from './useVolume';
+export { createHook, type HookDeclaration, type HookDeclarationContext } from './common';
