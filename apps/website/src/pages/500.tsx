962f5f563ea30118c7df6d03afd5841a02ea9b80
Simon <androz2091@gmail.com>
2023-06-24 17:26:46 +0200

    trigger commit

--- apps/website/src/pages/500.tsx
+++ apps/website/src/pages/500.tsx
@@ -7,7 +7,7 @@ export default function InternalError() {
         <Container className="min-h-[70vh] space-y-3 background">
             <div className="text-center">
                 <Heading.H1 className="text-9xl">500</Heading.H1>
-                <Heading.H3 className="font-normal text-muted-foreground">Something went wrong!</Heading.H3>
+                <Heading.H3 className="font-normal text-muted-foreground">Oops, something went wrong!</Heading.H3>
             </div>
             <Link href="/">
                 <Button>Return Home</Button>
