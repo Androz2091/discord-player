6bc1dc78bb7bde9496d727005e13b79689e443a6
skdhg <46562212+skdhg@users.noreply.github.com>
2023-06-25 18:58:45 +0545

    feat: icon

--- apps/website/src/pages/_app.tsx
+++ apps/website/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { EdgeUIProvider } from '@edge-ui/react';
 import { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import { AppLayout } from '@/components/layout/Layout';
+import Head from 'next/head';
 
 const inter = Inter({
     subsets: ['latin'],
@@ -14,6 +15,10 @@ const inter = Inter({
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+                <title>Discord Player</title>
+            </Head>
             <EdgeUIProvider fontSans={inter.style.fontFamily} theme="detect">
                 <AppLayout>
                     <Component {...pageProps} />
