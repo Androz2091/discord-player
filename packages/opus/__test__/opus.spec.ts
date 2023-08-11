2b757573a43863a043280090325e4aaca7e6f99a
skdhg <46562212+skdhg@users.noreply.github.com>
2023-08-11 20:21:33 +0545

    feat: @discord-player/opus

---
+++ packages/opus/__test__/opus.spec.ts
@@ -0,0 +1,8 @@
+import { OPUS_MOD_REGISTRY } from '../src';
+import { describe, it, expect } from 'vitest';
+
+describe('@discord-player/opus', () => {
+    it('should export opus module registry', () => {
+        expect(Array.isArray(OPUS_MOD_REGISTRY)).toBe(true);
+    });
+});
