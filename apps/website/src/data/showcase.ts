7e5d6cc4cf37a8b1a6ace82c556c1ebb9c117c05
iTsMaaT <102565513+iTsMaaT@users.noreply.github.com>
2024-11-03 19:11:15 -0500

    Update showcase.ts (#1946)

--- apps/website/src/data/showcase.ts
+++ apps/website/src/data/showcase.ts
@@ -176,6 +176,12 @@ export const ShowcaseResource: IShowcase = {
                 description: 'An open-source Discord music bot with an interative dashboard, only plays music, nothing else.',
                 version: 'v6.6.7',
                 url: 'https://muusik.app'
+            },
+            {
+                name: 'WD-40',
+                description: 'A music, utility & fun discord bot made in JavaScript ',
+                version: 'v6.7.1',
+                url: 'https://github.com/iTsMaaT/WD-40'
             }
         ].sort((a, b) => semver.rcompare(a.version, b.version))
     ),
